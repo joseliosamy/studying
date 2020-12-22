231df8104f6a89ac03c8dfa6449703c571b90d89
Josélio Samy <joseliobackup@gmail.com>
2020-12-22 00:57:05 -0300

    project one

---
+++ projeto01/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="keywords" content="palavras-chave,do,site">
+    <title>Projeto one</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logo</div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="banner-principal">
+        <form action="">
+            <h2>Qual o seu melhor email</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar">
+        </form>
+    </section> 
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Josélio Samy</h2>
+            <p>Sed nec sollicitudin dui, sit amet accumsan magna. Phasellus pulvinar sem lacus, in fermentum sem suscipit nec. Duis felis neque, mattis nec molestie nec, viverra ac odio. Etiam elementum imperdiet convallis. Suspendisse leo neque, volutpat non dictum ut, fermentum vel est.</p>
+            <p> Ut efficitur accumsan odio, ut lobortis risus pellentesque id. Duis tristique feugiat dolor, porta rhoncus leo pulvinar sit amet. Sed fringilla feugiat luctus. Ut ut malesuada ex. Integer porta nulla ipsum. Pellentesque vehicula blandit libero quis suscipit.</p>
+        </div>
+        <div class="w50">
+            <img src="images/yo.jpg" alt="">
+        </div>
+    </section>
+</body>
+</html>
